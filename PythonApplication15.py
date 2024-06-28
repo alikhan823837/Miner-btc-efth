865c95c7541265dfd26f9552d734ebb5e7ca8dfe
alikhan823837 <166453589+alikhan823837@users.noreply.github.com>
2024-06-28 23:26:56 +0600

    Add files via upload

---
+++ PythonApplication15.py
@@ -0,0 +1,19 @@
+﻿def main():
+    num1 = 7  
+    num2 = 5  
+
+    # Выводим задачу
+    print(f"еки саннын косындысын табыныз: {num1} + {num2} = ", end='')
+
+    # Считываем ответ пользователя
+    user_answer = int(input())
+
+    # Проверяем ответ
+    correct_answer = num1 + num2
+    if user_answer == correct_answer:
+        print("дурыс")
+    else:
+        print(f"дурыс емес. Дурыс жауып: {correct_answer}")
+
+if __name__ == "__main__":
+    main()
